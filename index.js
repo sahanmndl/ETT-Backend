b8fd2f6ff220c8e71df4a3c0656802f3131f035e
sahanmndl <mondalsahan053@gmail.com>
2024-08-24 02:24:34 +0530

    rerender

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ import {connectToCacheDB} from "./cache/redis.js";
 dotenv.config()
 
 const app = express();
-app.set('trust proxy', true)
-app.get('/ip', (request, response) => response.send(request.ip))
+// app.set('trust proxy', true)
 app.use(cors())
 app.use(helmet())
-// app.use(rateLimiter)
+app.use(rateLimiter)
 app.use(express.json({limit: "30mb", extended: true}))
 
 app.use('/', pingRoutes)
