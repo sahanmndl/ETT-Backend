14a43dcc1191dd7c4143b465c8ae4ca61cfc242f
sahanmndl <mondalsahan053@gmail.com>
2024-08-24 14:57:20 +0530

    vercel.json

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import {logger} from "./logger.js";
 
 export const connectMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+        await mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
             .then(() => logger.info("Connected to MongoDB"))
     } catch (e) {
         logger.error("Error connecting to MongoDB", e);
